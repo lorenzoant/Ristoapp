dca6a0e02db0f6a60c35865ee16ffcba9f9392c5
lorenzoant <35744993+lorenzoant@users.noreply.github.com>
2019-01-30 11:58:58 +0100

    fix

--- src/ristoapp/bean/AbbonamentoRistBean.java
+++ src/ristoapp/bean/AbbonamentoRistBean.java
@@ -1,8 +1,15 @@
 package ristoapp.bean;
 import java.sql.Date;
+import java.io.Serializable;
 import java.util.ArrayList;
 public class AbbonamentoRistBean implements java.io.Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3338316497127062328L;
 
 	
+
+
 }
