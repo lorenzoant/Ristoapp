822e0846a493e91d7f5664946106c7b69778281d
Redox00 <46685338+Redox00@users.noreply.github.com>
2019-05-13 19:34:57 +0200

    Update AccountServlet.java

--- src/ristoapp/servlet/AccountServlet.java
+++ src/ristoapp/servlet/AccountServlet.java
@@ -57,8 +57,6 @@ public class AccountServlet extends HttpServlet {
 			}
 			
 			
-			
-			
 			Boolean NotificaEmail = true; 
 			if(request.getParameter("NotificaEmail") == null) NotificaEmail = false;
 			
@@ -79,6 +77,7 @@ public class AccountServlet extends HttpServlet {
 				GeneraCodice codice = new GeneraCodice();
 			
 				cliente.setEmail(Email);
+				//cliente.setPassHash(PassHash);
 				cliente.setPassHash(encrypt.sha1(PassHash));
 				cliente.setCognome(Cognome);
 				cliente.setNome(Nome);
@@ -102,8 +101,9 @@ public class AccountServlet extends HttpServlet {
 				
 				SaveMySQL saveOnDb = new SaveMySQL();
 				try {
-					saveOnDb.inserisciCliente(cliente);
-					response.sendRedirect("login.jsp");
+					int esito = saveOnDb.inserisciCliente(cliente);
+					if(esito == 0)response.sendRedirect("login.jsp");
+					else request.setAttribute("errorMessage1", "errore1");
 				} catch (Exception e) {
 					e.printStackTrace();
 					sc = request.getSession().getServletContext();																								
